c388d4e1b8691819fb181fafeee7aa22599a10ca
Mel Beckerleg <beckerleg@maths.ox.ac.uk>
2016-10-18 17:54:57 +0100

    latestcode

--- InFoMM/YearOne/WeekTwo/ImageClassificationTask/KrisNMelCode.m
+++ InFoMM/YearOne/WeekTwo/ImageClassificationTask/KrisNMelCode.m
@@ -18,14 +18,37 @@ k=3
 IDX=knnsearch(trainimages',testimages(:,1:1000)','K',k);
 %then train(IDX(i,j)) is the jth closest image to the ith test image
 %takes a long time!
+%let's visualise the matched images for a particular test image.
+matchedpic1=zeros(784,4)
+for j=1:k
+matchedpic1(:,j)=trainimages(:,IDX(1,j));
+end
+pic1=reshape(testimages(:,1),28,28);
+imshow(pic1);
+matchedpic1=reshape(matchedpic1(:,1),28,28);
+imshow(matchedpic1);
+
 
 % Find the best using mode &See how well we've done for the nearest we've found:
-for i=1:10000
+for i=1:1000
     IDX1(i,:)= trainlabels(IDX(i,:));
     matching(i)=mode(IDX1(i,:)); %matching is the prediction for what the images are.
 v(i)=matching(i)-testlabels(i);
 
 end
+
+%what about a picture of one that went wrong?
+select=find(v);
+s=select(1);
+matchedpic2=zeros(784,4);
+for j=1:k
+matchedpic2(:,j)=trainimages(:,IDX(s,j));
+end
+pic2=reshape(testimages(:,s),28,28);
+imshow(pic2);
+ermatchedpic2=reshape(matchedpic2(:,2),28,28);
+imshow(ermatchedpic2);
+
 
 nnz(v) %gives the number of inaccurate values. 
 
@@ -50,8 +73,20 @@ end
 imshow(diffmean)
 
 % Work with test images
+k=3
+IDX=knnsearch(trainimagesdiff',testimagesdiff(:,1:1000)','K',k);
+%then train(IDX(i,j)) is the jth closest image to the ith test image
+%takes a long time!
 
-IDX = knnsearch(trainimagesdiff',testimagesdiff(1)', 'K',k) %comparing one
+% Find the best using mode &See how well we've done for the nearest we've found:
+for i=1:10000
+    IDX1(i,:)= trainlabels(IDX(i,:));
+    matching(i)=mode(IDX1(i,:)); %matching is the prediction for what the images are.
+v2(i)=matching(i)-testlabels(i);
+
+end
+
+nnz(v2) %gives the number of inaccurate values. 
 
 
 %% Principal Components Case
@@ -69,22 +104,31 @@ Xftest=Xftest(1:f,:);
 
 %knn algorithm for pca
 
-k=3
-idx=knnsearch(Xftrain',Xftest(:,1:1000)','K',1);
-%then train(IDX(i,j)) is the jth closest image to the ith test image
+k=4
+idx=knnsearch(Xftrain',Xftest(:,1:1000)','K',k);
+%then trainlabels(IDX(:,j)) is the jth closest image to the ith test image
+
 
 
 % Find the best using mode &See how well we've done for the nearest we've found:
+w=zeros(1000,1);
+idx1=zeros(1000,k);
 for i=1:1000
     idx1(i,:)= trainlabels(idx(i,:));
-    matching(i)=mode(idx1(i,:)); %matching is the prediction for what the images are.
-w(i)=matching(i)-testlabels(i);
+    matching(i)=mode(idx1(i,:)'); %matching is the prediction for what the images are.
+    w(i)=matching(i)-testlabels(i);
 
 end
 
 nnz(w)
 
 % 2.7% error
+%k=1 we get 
+%k=
+
+
+
+
 
 %% Principal Components Case with subtracted mean
 %eigendecomposition of the covariance matrix
@@ -115,6 +159,7 @@ wm(i)=matching(i)-testlabels(i);
 end
 
 nnz(wm)
+
 
 
 
