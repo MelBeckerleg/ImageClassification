93956a3ab1a70aca863d8cc681ac3f95ee40aaad
MelBeckerleg <beckerleg@maths.ox.ac.uk>
2016-10-21 13:48:46 +0100

    Update Intensityerrorcorr.m

--- InFoMM/YearOne/WeekTwo/ImageClassificationTask/Code/Intensityerrorcorr.m
+++ InFoMM/YearOne/WeekTwo/ImageClassificationTask/Code/Intensityerrorcorr.m
@@ -1,6 +1,6 @@
 function [w] = Intensity(k,n,'metric')
 %INTENSITY Outputs the number of errors in the image classification of n
-%test images, using the k-nearest neighbors algorithm and the specified metric.
+%test images, using the k-nearest neighbors algorithm and the specified metric, ie 'correlation'.
 
 %Read the binary files by converting them into .mat files:
 
